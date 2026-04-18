4ccd1054d5b9bab2fb0c125b4797426cfd2c7516
Justin Marshall <justinmarkark@gmail.com>
2026-04-18 17:58:53 +0100

    add a leaderboard

--- variables.tf
+++ variables.tf
@@ -1,4 +1,7 @@
-# class and type definitions
+variable "arcade_network_name" {
+  type    = string
+  default = "arcade-network"
+}
 
 variable "launcher_name" {
   type    = string
@@ -35,6 +38,63 @@ variable "portainer_volume_name" {
   default = "portainer_data"
 }
 
+variable "leaderboard_api_name" {
+  type    = string
+  default = "arcade-leaderboard-api"
+}
+
+variable "leaderboard_api_image" {
+  type    = string
+  default = "ghcr.io/justinmarkmarshall/arcade-leaderboard:latest"
+}
+
+variable "leaderboard_api_port" {
+  type    = number
+  default = 8081
+}
+
+variable "leaderboard_api_internal_port" {
+  type    = number
+  default = 8080
+}
+
+variable "leaderboard_db_name" {
+  type    = string
+  default = "arcade-leaderboard-db"
+}
+
+variable "leaderboard_db_image" {
+  type    = string
+  default = "postgres:16"
+}
+
+variable "leaderboard_db_port" {
+  type    = number
+  default = 5433
+}
+
+variable "leaderboard_db_database" {
+  type    = string
+  default = "arcade_leaderboard"
+}
+
+variable "leaderboard_db_username" {
+  type    = string
+  default = "postgres"
+}
+
+variable "leaderboard_db_password" {
+  type      = string
+  default   = "postgres"
+  sensitive = true
+}
+
+variable "leaderboard_db_volume_name" {
+  type        = string
+  default     = "arcade-leaderboard-data"
+  description = "Docker volume used to persist leaderboard PostgreSQL data. Protected from destroy so scores survive redeployments."
+}
+
 variable "games" {
   description = "Game container definitions"
   type = map(object({
@@ -45,3 +105,5 @@ variable "games" {
     description   = string
   }))
 }
+
+
